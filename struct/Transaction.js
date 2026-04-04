cf50595c1b0d6a033ad27bc56e72d1f2a584e4b7
vandam71 <rafaelsamorinha@gmail.com>
2026-04-05 00:53:22 +0100

    [FIX] #6 #13: Transaction.process() removes await+then mix; skip audit record if user not found

--- struct/Transaction.js
+++ struct/Transaction.js
@@ -10,10 +10,13 @@ module.exports = class Transaction {
     }
 
     async process() {
-        await User.findOneAndUpdate({id: this.id}, {$inc: {coins: this.value}}).then(async () => {
-            await _Transaction.create({user: this.id, value: this.value, description: this.reason});
-            logger.transaction(`${this.id.toString()} -> ${this.value}BC "${this.reason}"`);
-        });
+        const updated = await User.findOneAndUpdate({ id: this.id }, { $inc: { coins: this.value } });
+        if (!updated) {
+            logger.warn(`Transaction skipped: user ${this.id} not found in DB`);
+            return this.value;
+        }
+        await _Transaction.create({ user: this.id, value: this.value, description: this.reason });
+        logger.transaction(`${this.id.toString()} -> ${this.value}BC "${this.reason}"`);
         return this.value;
     }
 }
