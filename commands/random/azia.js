f1871115a1930ddbe249da5a367febbbf6e99b43
vandam71 <rafaelsamorinha@gmail.com>
2026-04-05 01:00:26 +0100

    [FIX] #22: azia.js validate user before applying cooldown

--- commands/random/azia.js
+++ commands/random/azia.js
@@ -14,13 +14,13 @@ module.exports = {
         const targetMember = interaction.options.getMember('user');
         const target = targetMember ? targetMember.user : interaction.user;
 
+        const user = await User.findOneAndUpdate({ id: target.id }, { $inc: { azia: 1 } });
+        if (!user)
+            return interaction.reply({ content: "This user hasn't talked in this server yet.", ephemeral: true });
+
         aziaRecently.add(interaction.user.id);
         setTimeout(() => { aziaRecently.delete(interaction.user.id); }, 60 * 1000);
 
-        User.findOneAndUpdate({ id: target.id }, { $inc: { azia: 1 } }).then(user => {
-            if (user === null)
-                return interaction.reply({ content: "This user hasn't talked in this server yet.", ephemeral: true });
-            return interaction.reply(`O <@${target.id}> já aziou ${user.azia + 1} vezes.`);
-        });
+        return interaction.reply(`O <@${target.id}> já aziou ${user.azia + 1} vezes.`);
     }
 };
