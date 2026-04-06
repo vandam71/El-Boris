56c0beceabe6a69ea72baf9b156656c041d3f194
vandam71 <rafaelsamorinha@gmail.com>
2026-04-06 09:24:15 +0100

    [UX] #22: fistbump accepts optional user to tag

--- commands/random/fistbump.js
+++ commands/random/fistbump.js
@@ -3,8 +3,11 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('fistbump')
-        .setDescription('Fist bumps someone'),
+        .setDescription('Fist bumps someone')
+        .addUserOption(opt => opt.setName('user').setDescription('User to fistbump').setRequired(false)),
     execute: async function (interaction, client) {
-        return interaction.reply('https://i.imgur.com/oL0XUD8.png');
+        const target = interaction.options.getUser('user');
+        const mention = target ? `<@${target.id}> ` : '';
+        return interaction.reply(`${mention}https://i.imgur.com/oL0XUD8.png`);
     }
 };
