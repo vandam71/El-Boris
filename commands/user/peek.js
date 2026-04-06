678e5fbe08209743397caef7cf38147fd63f46bf
vandam71 <rafaelsamorinha@gmail.com>
2026-04-06 09:23:23 +0100

    [UX] #17 #18: peek respects private flag, aligns stats with profile

--- commands/user/peek.js
+++ commands/user/peek.js
@@ -13,11 +13,14 @@ module.exports = {
         if (user === null) {
             return interaction.reply({ content: 'This user has no profile!', ephemeral: true });
         }
+        if (user.private) {
+            return interaction.reply({ content: 'This user has set their profile to private.', ephemeral: true });
+        }
         const embed = new EmbedBuilder()
             .setColor(0x00AE86)
             .setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL() })
             .setTitle(`${member.user.username} Profile`)
-            .addFields({ name: "Stats", value: `**Level: ${user.level}**\nAzia: **${user.azia}**` })
+            .addFields({ name: "Stats", value: `**Level: ${user.level}**\n<:xp:801554148994056202> Experience: **${user.xp}**\n<:boriscoin:798017751842291732> BorisCoins: **${user.coins}**\nAzia: **${user.azia}**` })
             .setThumbnail(member.user.avatarURL());
         return interaction.reply({ embeds: [embed] });
     }
